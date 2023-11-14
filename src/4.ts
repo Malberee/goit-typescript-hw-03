0712beb49dfbcc1c53c0232fe7d3f29eaf07b056
Malberee <82259650+Malberee@users.noreply.github.com>
2023-11-14 21:37:46 +0200

    save

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,51 @@
+class Key {
+    private signature = Math.random()
+
+    getSignature() {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {
+        this.key = key;
+    }
+
+    getKey() {
+        return this.key
+    }
+}
+
+abstract class House {
+    protected door: boolean = false
+    protected tenants: Person[]
+
+    constructor(protected key: Key) {
+        this.key = key
+    }
+
+    comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person)
+        }
+    }
+
+    abstract openDoor(key: Key): void
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key)
+        this.key = key
+    }
+
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
